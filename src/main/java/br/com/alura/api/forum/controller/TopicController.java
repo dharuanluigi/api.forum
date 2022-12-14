c079b6c5675e72c33e5a49915fb0ae9263109e39
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-14 14:07:41 -0300

    Added: insert topic feature

--- src/main/java/br/com/alura/api/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/api/forum/controller/TopicController.java
@@ -1,12 +1,14 @@
 package br.com.alura.api.forum.controller;
 
+import br.com.alura.api.forum.dto.AddedTopicDTO;
+import br.com.alura.api.forum.dto.InsertTopicDTO;
 import br.com.alura.api.forum.dto.ListTopicsDTO;
+import br.com.alura.api.forum.repository.CourseRepository;
 import br.com.alura.api.forum.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -17,12 +19,22 @@ public class TopicController {
     @Autowired
     private TopicRepository repository;
 
+    @Autowired
+    private CourseRepository courseRepository;
+
     @GetMapping
-    public List<ListTopicsDTO> findAll(@RequestParam(defaultValue = "course_name") String course_name) {
+    public List<ListTopicsDTO> findAllOrByCourseName(String course_name) {
         if (course_name != null) {
             return repository.findByCourse_Name(course_name);
         }
-
         return repository.findAll().stream().map(ListTopicsDTO::new).toList();
+    }
+
+    @PostMapping
+    public ResponseEntity<AddedTopicDTO> insert(@RequestBody InsertTopicDTO insertTopicDTO, UriComponentsBuilder uriBuilder) {
+        var topic = insertTopicDTO.toEntity(courseRepository);
+        repository.save(topic);
+        var uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
+        return ResponseEntity.created(uri).body(new AddedTopicDTO(topic));
     }
 }
