73ee2288fd341d56ee26a61f200888bb892e399a
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-21 20:04:13 -0300

    Added: find all feature

--- src/main/java/br/com/alura/api/forum/services/UserService.java
+++ src/main/java/br/com/alura/api/forum/services/UserService.java
@@ -2,11 +2,15 @@ package br.com.alura.api.forum.services;
 
 import br.com.alura.api.forum.dto.CreatedUserDTO;
 import br.com.alura.api.forum.dto.InsertUserDTO;
+import br.com.alura.api.forum.dto.ListProfileDTO;
+import br.com.alura.api.forum.dto.ListUserDTO;
 import br.com.alura.api.forum.entity.User;
 import br.com.alura.api.forum.repository.ProfileRepository;
 import br.com.alura.api.forum.repository.UserRepository;
 import br.com.alura.api.forum.services.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -27,5 +31,12 @@ public class UserService implements IUserService {
         var user = userRepository.save(new User(null, insertUserDTO.name(), insertUserDTO.email(),
                 new BCryptPasswordEncoder(10).encode(insertUserDTO.password()), List.of(profiles)));
         return new CreatedUserDTO(user);
+    }
+
+    @Override
+    public Page<ListUserDTO> findAll(Pageable pagination) {
+        var foundedUsers = userRepository.findAll(pagination);
+        return foundedUsers.map(u -> new ListUserDTO(u.getId(), u.getEmail(),
+                u.getProfiles().stream().map(uu -> new ListProfileDTO(uu.getName())).toList()));
     }
 }
