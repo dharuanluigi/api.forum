aa8e58e38cb2d0f275115096e6a8872c2f1aa436
Dharuan Luigi <dharuanluigi@gmail.com>
2022-12-30 11:11:16 -0300

    Refactor: move responsibility of authentication to a service dedicated

--- src/main/java/br/com/alura/api/forum/controller/AuthenticationController.java
+++ src/main/java/br/com/alura/api/forum/controller/AuthenticationController.java
@@ -2,14 +2,13 @@ package br.com.alura.api.forum.controller;
 
 import br.com.alura.api.forum.dto.LoginDTO;
 import br.com.alura.api.forum.dto.TokenDTO;
-import br.com.alura.api.forum.entity.User;
+import br.com.alura.api.forum.exceptions.ForbiddenRequestException;
+import br.com.alura.api.forum.service.interfaces.IAuthenticationService;
 import br.com.alura.api.forum.service.interfaces.ITokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Profile({"prod", "test"})
 public class AuthenticationController {
     @Autowired
-    private ITokenService tokenService;
+    private IAuthenticationService authenticationService;
 
     @Autowired
-    private AuthenticationManager authenticationManager;
+    private ITokenService tokenService;
 
     @PostMapping
-    public ResponseEntity<TokenDTO> authenticate(@RequestBody @Valid LoginDTO loginDto) {
-        var userToken = new UsernamePasswordAuthenticationToken(loginDto.email(), loginDto.password());
-        var authentication = authenticationManager.authenticate(userToken);
-        var token = tokenService.create((User) authentication.getPrincipal());
-        return ResponseEntity.ok(new TokenDTO("Bearer", token));
+    public ResponseEntity<TokenDTO> login(@RequestBody @Valid LoginDTO loginDto) {
+        var user = authenticationService.loginUser(loginDto.email(), loginDto.password());
+
+        if (user != null && user.getIsActive()) {
+            var token = tokenService.create(user);
+            return ResponseEntity.ok(new TokenDTO("Bearer", token));
+        }
+
+        throw new ForbiddenRequestException("User can't make login, contact system admin");
     }
 }
