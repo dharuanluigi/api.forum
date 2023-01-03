3b01322374800731feedc9d649c21682b28d8fd9
Dharuan Luigi <dharuanluigi@gmail.com>
2023-01-03 12:37:47 -0300

    Added: configuration auth for swagger

--- src/main/java/br/com/alura/api/forum/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/alura/api/forum/configuration/SwaggerConfiguration.java
@@ -1,9 +1,11 @@
 package br.com.alura.api.forum.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +15,8 @@ public class SwaggerConfiguration {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(getApiInfo());
+                .info(getApiInfo())
+                .components(getSecurityComponents());
     }
 
     private Info getApiInfo() {
@@ -34,5 +37,10 @@ public class SwaggerConfiguration {
                 .name("Dharuan Emina")
                 .email("dharuanluigi@gmail.com")
                 .url("https://github.com/dharuanluigi");
+    }
+
+    private Components getSecurityComponents() {
+        return new Components()
+                .addSecuritySchemes("Authorization", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
     }
 }
--- src/main/java/br/com/alura/api/forum/controller/UserController.java
+++ src/main/java/br/com/alura/api/forum/controller/UserController.java
@@ -4,6 +4,7 @@ import br.com.alura.api.forum.dto.*;
 import br.com.alura.api.forum.service.interfaces.IEmailService;
 import br.com.alura.api.forum.service.interfaces.IUserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequestMapping("/users")
 @Tag(name = "User", description = "Manage what features the user can doing")
 public class UserController {
+
+    private final String SECURITY_HEADER_VALUE = "Authorization";
 
     @Autowired
     private IUserService userService;
@@ -40,30 +43,35 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(security = {@SecurityRequirement(name = SECURITY_HEADER_VALUE)})
     public ResponseEntity<Page<DetailsUserDTO>> findAll(@PageableDefault Pageable pagination) {
         var allUsers = userService.findAll(pagination);
         return ResponseEntity.ok(allUsers);
     }
 
     @GetMapping("/{id}")
+    @Operation(security = {@SecurityRequirement(name = SECURITY_HEADER_VALUE)})
     public ResponseEntity<DetailsUserBaseDTO> findById(@PathVariable String id) {
         var foundedUser = userService.findById(id);
         return ResponseEntity.ok(foundedUser);
     }
 
     @GetMapping("/me")
+    @Operation(security = {@SecurityRequirement(name = SECURITY_HEADER_VALUE)})
     public ResponseEntity<DetailsOwnUserDTO> getDataCurrentLoggedUser() {
         var userData = userService.getCurrentUserData();
         return ResponseEntity.ok(userData);
     }
 
     @PutMapping("/{id}")
+    @Operation(security = {@SecurityRequirement(name = SECURITY_HEADER_VALUE)})
     public ResponseEntity<UpdatedUserDTO> update(@PathVariable String id, @RequestBody UpdateUserDTO updateUserDTO) {
         var user = userService.update(id, updateUserDTO);
         return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(security = {@SecurityRequirement(name = SECURITY_HEADER_VALUE)})
     public ResponseEntity<Void> delete(@PathVariable String id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
