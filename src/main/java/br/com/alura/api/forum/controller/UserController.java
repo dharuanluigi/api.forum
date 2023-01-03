2e7137c7a0ba35fd426936656f6bfd13200c210e
Dharuan Luigi <dharuanluigi@gmail.com>
2023-01-03 19:26:05 -0300

    Added: notations to documentation in swagger

--- src/main/java/br/com/alura/api/forum/controller/UserController.java
+++ src/main/java/br/com/alura/api/forum/controller/UserController.java
@@ -4,7 +4,11 @@ import br.com.alura.api.forum.dto.*;
 import br.com.alura.api.forum.service.interfaces.IEmailService;
 import br.com.alura.api.forum.service.interfaces.IUserService;
 import br.com.alura.api.forum.util.Constants;
+import br.com.alura.api.forum.wrapper.PageDetailsUserWrapper;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -27,7 +31,8 @@ public class UserController {
     private IEmailService emailService;
 
     @PostMapping
-    @Operation(summary = "Creates an user", description = "Endpoint to creates a user access into forum by unique email")
+    @Operation(summary = "Creates a user", description = "Endpoint to creates a user access into forum by unique email")
+    @ApiResponse(responseCode = "409", description = "When a user was found already registered with the same username or email", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<CreatedUserDTO> create(@RequestBody @Valid InsertUserDTO insertUserDTO,
                                                  UriComponentsBuilder uriBuilder) {
         var user = userService.create(insertUserDTO);
@@ -42,6 +47,8 @@ public class UserController {
 
     @GetMapping
     @Operation(security = {@SecurityRequirement(name = Constants.SECURITY_HEADER_VALUE)}, summary = "Find all user with filter", description = "Possible see all usernames with their roles")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = PageDetailsUserWrapper.class)))
+    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<Page<DetailsUserDTO>> findAll(@PageableDefault Pageable pagination) {
         var allUsers = userService.findAll(pagination);
         return ResponseEntity.ok(allUsers);
@@ -49,6 +56,9 @@ public class UserController {
 
     @GetMapping("/{id}")
     @Operation(security = {@SecurityRequirement(name = Constants.SECURITY_HEADER_VALUE)}, summary = "Find a user by their id", description = "See more details about a user")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = DetailsUserBaseDTO.class)))
+    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<DetailsUserBaseDTO> findById(@PathVariable String id) {
         var foundedUser = userService.findById(id);
         return ResponseEntity.ok(foundedUser);
@@ -56,6 +66,8 @@ public class UserController {
 
     @GetMapping("/me")
     @Operation(security = {@SecurityRequirement(name = Constants.SECURITY_HEADER_VALUE)}, summary = "Get current user data", description = "Possible see all data about your own logged user")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = DetailsOwnUserDTO.class)))
+    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<DetailsOwnUserDTO> getDataCurrentLoggedUser() {
         var userData = userService.getCurrentUserData();
         return ResponseEntity.ok(userData);
@@ -63,6 +75,9 @@ public class UserController {
 
     @PutMapping("/{id}")
     @Operation(security = {@SecurityRequirement(name = Constants.SECURITY_HEADER_VALUE)}, summary = "Update your data", description = "Possible update just your own data, includes moderators role")
+    @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = UpdatedUserDTO.class)))
+    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<UpdatedUserDTO> update(@PathVariable String id, @RequestBody UpdateUserDTO updateUserDTO) {
         var user = userService.update(id, updateUserDTO);
         return ResponseEntity.ok(user);
@@ -70,6 +85,9 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     @Operation(security = {@SecurityRequirement(name = Constants.SECURITY_HEADER_VALUE)}, summary = "Delete account", description = "Possible delete your own account and moderators can delete the other ones")
+    @ApiResponse(responseCode = "204", description = "No content")
+    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<Void> delete(@PathVariable String id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
@@ -77,6 +95,7 @@ public class UserController {
 
     @PatchMapping("/active")
     @Operation(summary = "Active account", description = "Used to active an account by informed code that was send in informed registered email")
+    @ApiResponse(responseCode = "202", description = "Accepted")
     public ResponseEntity<Void> active(@RequestHeader String code) {
         userService.activate(code);
         return ResponseEntity.accepted().build();
@@ -84,6 +103,8 @@ public class UserController {
 
     @PostMapping("/resend-code")
     @Operation(summary = "Resend verification code", description = "If user has the your code expired or an old account should be activated again to receive new code activation")
+    @ApiResponse(responseCode = "202", description = "Accepted")
+    @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
     public ResponseEntity<Void> resendVerificationCode(@RequestBody LoginDTO loginDTO) {
         userService.resendActivationCode(loginDTO.email(), loginDTO.password());
         return ResponseEntity.accepted().build();
